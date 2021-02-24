959f766939f8b2b296cff620026a556681537cde
tasteFinanceDev <tastefinance@gmail.com>
2021-02-24 12:08:43 -0600

    add voting

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -49,6 +49,10 @@ const config: MenuEntry[] = [
     icon: 'MoreIcon',
     items: [
       {
+        label: "Voting",
+        href: "https://snapshot.page/#/tastefinance.eth/",
+      },      
+      {
         label: "Github",
         href: "https://github.com/TasteFinance/",
       },
